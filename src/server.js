64b8fa5641d2a31d017f42cf7391f23d8082cd5b
alihfph <ali.hfph@gmail.com>
2021-04-15 19:30:01 +0200

    uploadfile

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 import express from "express";
 import listEndpoints from "express-list-endpoints";
 import cors from "cors";
-
+import { join } from "path";
+import filesRoutes from "./files/index.js";
 import studentsRoutes from "./students/index.js";
 import {
   badRequestErrorHandler,
@@ -9,13 +10,19 @@ import {
   forbiddenErrorHandler,
   catchAllErrorHandler,
 } from "./errorHandling.js";
+import { getCurrentFolderPath } from "./lib/fs-tools.js"
 
 const server = express();
 const port = 3001;
-
+const publicFolderPath = join(
+  getCurrentFolderPath(import.meta.url),
+  "../public"
+);
+server.use(express.static(publicFolderPath));
 server.use(cors());
 server.use(express.json());
 server.use("/projects", studentsRoutes);
+server.use("/files", filesRoutes);
 
 server.use(badRequestErrorHandler);
 server.use(notFoundErrorHandler);
