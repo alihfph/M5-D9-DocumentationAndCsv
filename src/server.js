e0b61c47aec926b0988feea74d898d96d7960f5d
alihfph <ali.hfph@gmail.com>
2021-04-20 23:17:24 +0200

    app

--- src/server.js
+++ src/server.js
@@ -10,16 +10,32 @@ import {
   forbiddenErrorHandler,
   catchAllErrorHandler,
 } from "./errorHandling.js";
-import { getCurrentFolderPath } from "./lib/fs-tools.js"
+import { getCurrentFolderPath } from "./lib/fs-tools.js";
 
 const server = express();
-const port = 3001;
+const port = process.env.PORT || 3000;
+const whitelist = [process.env.FE_URL_DEV, process.env.FE_URL_PROD];
+const corsOptions = {
+  origin: function (origin, next) {
+    if (whitelist.indexOf(origin) !== -1) {
+      console.log("ORIGIN ", origin);
+      // origin found in whitelist
+      next(null, true);
+    } else {
+      // origin not found in the whitelist
+      next(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
+
 const publicFolderPath = join(
   getCurrentFolderPath(import.meta.url),
   "../public"
 );
 server.use(express.static(publicFolderPath));
-server.use(cors());
+
 server.use(express.json());
 server.use("/projects", studentsRoutes);
 server.use("/files", filesRoutes);
@@ -30,6 +46,11 @@ server.use(forbiddenErrorHandler);
 server.use(catchAllErrorHandler);
 
 console.log(listEndpoints(server));
-server.listen(port, () => {
-  console.log("Server is running on port ", port);
+app.listen(port, () => {
+  if (process.env.NODE_ENV === "production") {
+    // no need to configure it manually on Heroku
+    console.log("Server running on cloud on port: ", port);
+  } else {
+    console.log("Server running locally on port: ", port);
+  }
 });
